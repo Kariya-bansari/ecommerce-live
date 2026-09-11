6a19c68fde0b18ee80d800bd4fb5114a1460843f
kariya prince <princekariya2002@gmail.com>
2026-09-11 11:49:40 +0530

    Drop legacy foreign key constraints and turn off DEBUG

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -4,3 +4,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.db import connection
+    with connection.cursor() as cursor:
+        cursor.execute("ALTER TABLE store_orderitem DROP CONSTRAINT IF EXISTS store_orderitem_product_id_f2b098d4_fk_store_product_id;")
+        cursor.execute("ALTER TABLE store_cart DROP CONSTRAINT IF EXISTS store_cart_product_id_41a1a5b8_fk_store_product_id;")
+        cursor.execute("ALTER TABLE store_wishlist DROP CONSTRAINT IF EXISTS store_wishlist_product_id_f95cd32d_fk_store_product_id;")
+except Exception as e:
+    print("Could not drop constraint:", e)
